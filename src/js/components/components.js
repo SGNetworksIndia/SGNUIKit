f21e601af87f41738b84feef96412cc340dbabf7
Sagnik Ganguly <sagnikganguly2012@rediffmail.com>
2023-01-02 16:23:28 +0530

    ADDED: Component: Windows: Windows can be initialized using '.sgn-window' CSS class

--- src/js/components/components.js
+++ src/js/components/components.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 SGNetworks. All rights reserved.
+ * Copyright (c) 2022-2023 SGNetworks. All rights reserved.
  *
  * The software is an exclusive copyright of "SGNetworks" and is provided as is exclusively with only "USAGE" access. "Modification",  "Alteration", "Re-distribution" is completely prohibited.
  * VIOLATING THE ABOVE TERMS IS A PUNISHABLE OFFENSE WHICH MAY LEAD TO LEGAL CONSEQUENCES.
@@ -11,5 +11,6 @@ import('./marquee.js');
 import('./modals.js');
 import('./sidebar.js');
 import('./wizard.js');
-import('./tablayout.js');
+import("./tablayout.js");
+import("./windows.js");
 
